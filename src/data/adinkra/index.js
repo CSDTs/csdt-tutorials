8c90e8bc392553821eb07b93976aef6ee49590bc
Andrew Hunn <ahunn59@gmail.com>
2022-09-20 09:11:24 -0400

    (chore) add remainder of baseline media for tutorials

---
+++ src/data/adinkra/index.js
@@ -0,0 +1,13 @@
+import AdinkraAkoma from "./adinkra-akoma.json";
+import AdinkraAnanse from "./adinkra-ananse.json";
+import AdinkraMpuannum from "./adinkra-mpuannum.json";
+import AdinkraSepow from "./adinkra-sepow.json";
+
+const Adinkra = {
+	"adinkra-akoma": AdinkraAkoma,
+	"adinkra-ananse": AdinkraAnanse,
+	"adinkra-mpuannum": AdinkraMpuannum,
+	"adinkra-sepow": AdinkraSepow,
+};
+
+export { Adinkra };
--- src/data/index.js
+++ src/data/index.js
@@ -1,14 +1,15 @@
 import BeadLoomProgramming from "./beadloom/beadloom-programming.json";
 
+import { Adinkra } from "./adinkra";
 import { Cornrows } from "./cornrows";
 import { Quilting } from "./quilting";
 import { TooledLeather } from "./tooledleather";
-
 import { YarnArts } from "./yarnarts";
 export const AvailableSets = {
 	...TooledLeather,
 	...Quilting,
 	...Cornrows,
 	...YarnArts,
+	...Adinkra,
 	"beadloom-programming": BeadLoomProgramming,
 };
